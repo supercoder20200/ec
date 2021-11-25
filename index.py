cf7d1e73f2a2d0fed2f6928123abde5448550de3
imraj <imraj3@live.co.uk>
2021-11-25 18:03:54 +0100

    .

--- index.py
+++ index.py
@@ -4,40 +4,72 @@ import plotly
 import plotly.graph_objs as go
 import plotly.express as px
 import numpy as np 
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib.backends.backend_svg import FigureCanvasSVG
 from matplotlib.figure import Figure
+import matplotlib.ticker as mtick
+from matplotlib.ticker import (AutoMinorLocator, MultipleLocator)
 import json
 import io
 import random
+import base64 
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    fig = Figure()
-    axis = fig.add_subplot(1,1,1)
-    x_points = range(50)
-    axis.plot(x_points, [random.randint(1, 30) for x in x_points])
+    img = io.BytesIO()
 
-    output = io.BytesIO()
-    FigureCanvasAgg(fig).print_png(output)
+    DefaultPx = 76713794182478803891528803692822015505111471502816304530189645921738551553824
+    DefaultPy = 9989913769501776956673418849599695616596036032394306022682365229231505097463
+    DefaultQx = 49396472789695839840842700318753929600130929079169291199003099305862335813206
+    DefaultQy = 6721877582892931413141315114289651970823246064430791708367976298368088092295
+    Rx, Ry = RValue( (DefaultPx, DefaultPy), (DefaultQx, DefaultQy) )
 
-    return render_template('modk-add.html', output=output.getvalue())
+    DefaultGx = 55066263022277343669578718895168534326250603453777594175500187360389116729240
+    DefaultGy = 32670510020758816978083085130507043184471273380659243275938904335757337482424
 
-# Test Image Plot
-@app.route('/plot_png')
-def plot_png():
-    fig = Figure()
-    axis = fig.add_subplot(1,1,1)
-    x_points = range(50)
-    axis.plot(x_points, [random.randint(1, 30) for x in x_points])
+    a = -1
+    b = 1
+    min = -1 * 10**78
+    max =  1.2 *  10**78
+    
+    y,x = np.ogrid[ min:max:10**78, min:max:10**78 ]
+    print("Ravel")
 
-    output = io.BytesIO()
-    FigureCanvasAgg(fig).print_png(output)
-    return Response(output.getvalue(), mimetype="image/png")
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
 
+    plt.contour(x.ravel(), y.ravel(), y**2 - x**3 - x * a -b, [0])
+    plt.grid()
+    
+    ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.0e'))
+    ax.xaxis.set_major_formatter(mtick.FormatStrFormatter('%.0e'))
+    ax.tick_params(axis='both', which='major', labelsize=6)
+
+    #ax.set_xticks( np.arange(min,max, 1 * 10**77) )
+    #ax.set_xticks( np.arange(min,max, 0.1 * 10**77), minor=True )
+    #ax.xaxis.set_minor_locator(AutoMinorLocator(4))
+    #ax.yaxis.set_minor_locator(AutoMinorLocator(4))
+
+    plt.annotate("P", (DefaultPx, DefaultPy))
+    plt.annotate("Q", (DefaultQx, DefaultQy))
+    plt.annotate("R", (Rx, Ry))
+    plt.annotate("G", (DefaultGx, DefaultGx))
+
+    sx = [DefaultPx, DefaultQx, DefaultGx]
+    sy = [DefaultPy, DefaultQy, DefaultGy]
+
+    plt.savefig(img, format='png')
+    plt.close()
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode('utf8')
+
+    return render_template('modk-add.html', figure=plot_url)
 
 def inverse_mod(k, p):
     #p = 2 ** 256 - 2 ** 32 - 2 ** 9 - 2 ** 8 - 2 ** 7 - 2 ** 6 - 2 ** 4 - 1
@@ -62,9 +94,27 @@ def inverse_mod(k, p):
 
     return x % p
 
+def RValue(P,Q):
+    p = 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f
+    Px,Py = P 
+    Qx,Qy = Q
+    if Px == Qx:
+        Delta = (3 *  Px * Px ) * inverse_mod(2 * Py, p)
+    else:
+        Delta = (Py - Qy) * inverse_mod(Px - Qx, p)
+
+    Rx = ( Delta * Delta - Px - Qx ) % p
+    Ry = Py + Delta * (Rx - Px)
+    Ry = -Ry % p
+    return (Rx,Ry)
+
+
 @app.route("/_mod_addition")
 def mod_add():
     # Modulus
+
+    img = io.BytesIO()
+
     #p = 2 ** 256 - 2 ** 32 - 2 ** 9 - 2 ** 8 - 2 ** 7 - 2 ** 6 - 2 ** 4 - 1
     p = 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f
     Px = int(request.args.get('px',0,int))
@@ -77,13 +127,51 @@ def mod_add():
     else:
         Delta = (Py - Qy) * inverse_mod(Px - Qx, p)
 
-    #Slope = (3x^2 + a) / 2y
-    #Delta = ( (Py - Qy) * inverse_mod( (Px - Qx), p) ) 
     Rx = ( Delta * Delta - Px - Qx ) % p
     Ry = Py + Delta * (Rx - Px)
     Ry = -Ry % p
+
+    a = -1
+    b = 1
+    min = -1 * 10**78
+    max =  1.2 *  10**78
     
-    return jsonify({'rx': str(Rx), 'ry': str(Ry) })
+    y,x = np.ogrid[ min:max:10**78, min:max:10**78 ]
+    print("Ravel")
+    #print(x.ravel())
+    #print(y.ravel())
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    plt.contour(x.ravel(), y.ravel(), y**2 - x**3 - x * a -b, [0])
+    plt.grid()
+    
+    ax.yaxis.set_major_formatter(mtick.FormatStrFormatter('%.0e'))
+    ax.xaxis.set_major_formatter(mtick.FormatStrFormatter('%.0e'))
+
+    plt.annotate("P", (DefaultPx, DefaultPy))
+    plt.annotate("Q", (DefaultQx, DefaultQy))
+    plt.annotate("R", (Rx, Ry))
+    plt.annotate("G", (DefaultGx, DefaultGx))
+
+    sx = [DefaultPx, DefaultQx, DefaultGx]
+    sy = [DefaultPy, DefaultQy, DefaultGy]
+
+    plt.savefig(img, format='png')
+    plt.close()
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode('utf8')
+    
+    return jsonify({'rx': str(Rx), 'ry': str(Ry), 'figure': plot_url  })
+
+def is_on_curve(self, x, y):
+    a = 0
+    b = 7
+    p = 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f 
+    if x is None or y is None:
+        return True
+    return (y * y - x * x * x - a * x - b) % p == 0
 
 def y_value(x):
     return sqrt(x**3 + 7)
