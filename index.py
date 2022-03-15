1b4ca3bce92d1acfe74485ea3eb708689a08bfba
imraj <imraj3@live.co.uk>
2022-03-15 16:31:26 +0100

    initial nvalue

--- index.py
+++ index.py
@@ -478,7 +478,7 @@ def RValue1(P,Q):
     return (Rx,Ry)
 
 def RValue1_mod2p(P,Q):
-    p = 2 * PValue
+    p = 2 * NValue
     
     Px, Py = P 
     Qx, Qy = Q
@@ -605,8 +605,8 @@ def RValue_mod2p(P,Q):
     Px, Py = P
     Qx, Qy = Q
 
-    p1 = Point(2 * PValue, Px, Py)
-    p2 = Point(2 * PValue, Qx, Qy)
+    p1 = Point(2 * NValue, Px, Py)
+    p2 = Point(2 * NValue, Qx, Qy)
 
     (x,y) = p1 + p2
 
